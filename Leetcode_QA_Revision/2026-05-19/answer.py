87677a0297b539b4f3cd99b7349117780a1d1afb
Vigneshwara <52130029+VigneshwaraChinnadurai@users.noreply.github.com>
2026-05-19 07:00:00 +0530

    Add LeetCode QA revision for 2026-05-19

---
+++ Leetcode_QA_Revision/2026-05-19/answer.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getCommon(self, nums1: list[int], nums2: list[int]) -> int:
+        i, j = 0, 0
+        n1, n2 = len(nums1), len(nums2)
+
+        while i < n1 and j < n2:
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else: # nums1[i] > nums2[j]
+                j += 1
+        
+        return -1
